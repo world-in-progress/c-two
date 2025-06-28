f592c717cf8981b80284485f072d56aec88a75ea
Soku <36831178+YcSoku@users.noreply.github.com>
2025-06-29 00:26:59 +0800

    refactor: add memory routing functionality and improve routing logic (#57)

---
+++ src/c_two/rpc/routing.py
@@ -0,0 +1,13 @@
+from .memory.memory_routing import memory_routing
+
+async def routing(server_address: str, event_bytes: bytes, timeout: float = 10.0) -> bytes:
+    """Relay event bytes to server and get response bytes asynchronously."""
+    if server_address.startswith(('tcp://', 'ipc://')):
+        return # TODO: Implement ZMQ routing
+    elif server_address.startswith('http://'):
+        return # TODO: Implement HTTP routing
+    elif server_address.startswith('memory://'):
+        return memory_routing(server_address, event_bytes, timeout)
+    else:
+        # TODO: Handle other protocols if needed
+        raise ValueError(f'Unsupported protocol in server_address: {server_address}')
